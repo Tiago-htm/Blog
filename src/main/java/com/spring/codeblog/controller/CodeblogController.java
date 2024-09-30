d5df49fffe4ff9771de65bc31aead8a254744564
Tiago-htm <tiagovilela019@gmail.com>
2024-09-30 19:55:28 -0300

    Estrutura de post completa e adição de navegação via titulo para outra pagina com id resgatado no banco

--- src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.spring.codeblog.service.CodeblogService;
-import com.spring.codeblog.model.Post;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.codeblog.model.Post;
+import com.spring.codeblog.service.CodeblogService;
 
 
 @Controller
@@ -20,13 +20,22 @@ public class CodeblogController {
     CodeblogService  codeblogService;
 
     @RequestMapping(value = "/posts", method=RequestMethod.GET)
-
-    
+ 
     public ModelAndView getPosts() {
         ModelAndView mv  = new ModelAndView("posts");
 
         List<Post> posts = codeblogService.findAll(); // retorna a lista de post
         mv.addObject("posts", posts); // objeto que referencia os post
+        return mv;
+
+    }
+
+    @RequestMapping(value = "/posts/{id}", method=RequestMethod.GET)
+
+    public ModelAndView getPostDetails(@PathVariable("id") long id) {
+        ModelAndView mv  = new ModelAndView("postsDetails");
+        Post post = codeblogService.findById(id);  //pegar o id 
+        mv.addObject("post", post); 
         return mv;
 
     }
