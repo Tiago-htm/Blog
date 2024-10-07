2fdac1bf010cffbbc1a5d23874e5b53da7c2723f
Tiago-htm <bueloslef@gmail.com>
2024-10-07 14:09:06 -0300

    Configurado login de acesso

---
+++ src/main/java/com/spring/codeblog/configuration/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.spring.codeblog.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private static final String[] AUTH_LIST = {
+            "/", "/posts", "/posts/{id}" // lista de urls que podem ser acessado sem o login
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(AUTH_LIST).permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(login -> login.permitAll())
+                .logout(logout -> logout.logoutUrl("/logout"));
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("Tiago")
+                .password(passwordEncoder().encode("123"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
